794d3f9256e65b81aa1e4f44762c0a0586e58a4b
muhammad-adeel <muhammad.adeel@codefulcrum.com>
2024-03-29 16:32:05 +0500

    pinia store configuration

---
+++ src/store/useTodoList.js
@@ -0,0 +1,36 @@
+import { defineStore } from "pinia";
+import { reactive } from "vue";
+
+const initialState = {
+  todoList: [],
+};
+
+export const useTodoList = defineStore("todoList", {
+  state: () =>
+    reactive({
+      todoList: [],
+      inputVal: "task1",
+    }),
+  getters: {},
+  actions: {
+    addItem() {
+      const prev = [...this.todoList];
+      const newItem = {
+        name: this.inputVal,
+        createdAt: new Date(),
+        isCompleted: false,
+      };
+      this.todoList = [...prev, newItem];
+      this.inputVal = "";
+    },
+    handleStatus(index) {
+      this.todoList[index].isCompleted = !this.todoList[index].isCompleted;
+    },
+    deleteTask(index) {
+      this.todoList.splice(index, 1);
+    },
+    updateTask(index) {
+      this.todoList[index].name = this.inputVal;
+    },
+  },
+});
